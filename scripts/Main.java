02dd4f624e255428bd4428dae2050916642d8534
Tavi <tavi@divested.dev>
2024-05-22 19:36:10 -0400

    Ability to scan screen content for malicious links
    Signed-off-by: Tavi <tavi@divested.dev>

--- scripts/Main.java
+++ scripts/Main.java
@@ -41,9 +41,12 @@ public class Main {
     private static BloomFilter<String> signaturesMD5 = null;
     private static BloomFilter<String> signaturesSHA1 = null;
     private static BloomFilter<String> signaturesSHA256 = null;
+    private static BloomFilter<String> domains = null;
 
     private static int amtLinesValid = 0;
     private static int amtLinesInvalid = 0;
+
+    private static int amtDomains = 0;
 
     private static int amtSignaturesReadMD5 = 0;
     private static int amtSignaturesReadSHA1 = 0;
@@ -66,24 +69,26 @@ public class Main {
         extendedMode = args[0].contains("-extended");
         //isFileInNsrl("B61905308B336AD268A782790B661616");
         int amtMaxMD5 = 7200000; //7.2m
-        if(extendedMode) {
-                amtMaxMD5 = 52000000; //52m
+        if (extendedMode) {
+            amtMaxMD5 = 52000000; //52m
         }
         int amtMaxSHA1 = 50000; //50k
         int amtMaxSHA256 = 2000000; //2m
+        int amtMaxDomains = 1800000; //1.8m
         signaturesMD5 = BloomFilter.create(Funnels.stringFunnel(Charsets.US_ASCII), amtMaxMD5, 0.00001);
         signaturesSHA1 = BloomFilter.create(Funnels.stringFunnel(Charsets.US_ASCII), amtMaxSHA1, 0.00001);
         signaturesSHA256 = BloomFilter.create(Funnels.stringFunnel(Charsets.US_ASCII), amtMaxSHA256, 0.00001);
+        domains = BloomFilter.create(Funnels.stringFunnel(Charsets.US_ASCII), amtMaxDomains, 0.00001);
         File existingDatabase = new File(args[0] + "../production/hypatia-md5-bloom.bin");
-        if(extendedMode && existingDatabase.exists()) {
-                try {
+        if (extendedMode && existingDatabase.exists()) {
+            try {
                 System.out.println("Loading existing hypatia-md5-bloom.bin database");
                 FileInputStream databaseLoading = new FileInputStream(existingDatabase);
                 signaturesMD5Dedupe = BloomFilter.readFrom(databaseLoading, Funnels.stringFunnel(Charsets.US_ASCII));
                 System.out.println("\tLoaded " + signaturesMD5Dedupe.approximateElementCount() + " entries");
-                } catch (Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
-                }
+            }
         }
 
         System.out.println("Processing exclusions:");
@@ -93,12 +98,12 @@ public class Main {
             try {
                 System.out.println("\t" + exclusionDatabase.getName());
                 Scanner s = new Scanner(exclusionDatabase);
-                while(s.hasNextLine()) {
+                while (s.hasNextLine()) {
                     String line = s.nextLine().trim().toLowerCase();
-                    if(line.contains(":")) {
+                    if (line.contains(":")) {
                         line = line.split(":")[0];
                     }
-                    if(!line.startsWith("#") && isHexadecimal(line) && (line.length() == 32 || line.length() == 40 || line.length() == 64)) {
+                    if (!line.startsWith("#") && isHexadecimal(line) && (line.length() == 32 || line.length() == 40 || line.length() == 64)) {
                         arrExclusions.add(line);
                         //System.out.println("\t\tAdded: " + line);
                     }
@@ -108,17 +113,37 @@ public class Main {
                 e.printStackTrace();
             }
         }
-        System.out.println("Loaded " + arrExclusions.size() + " excluded hashes");
+        System.out.println("\tLoaded " + arrExclusions.size() + " excluded hashes");
+
+        if (args.length == 2) {
+            System.out.println("Processing domains:");
+            File domainDatabase = new File(args[1]);
+            if (domainDatabase.exists()) {
+                try {
+                    Scanner s = new Scanner(domainDatabase);
+                    while (s.hasNextLine()) {
+                        String line = s.nextLine().trim().toLowerCase();
+                        if (!line.startsWith("#")) {
+                            domains.put(line);
+                        }
+                    }
+                    s.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("\tLoaded " + domains.approximateElementCount() + " domains");
+        }
 
         System.out.println("Processing signatures:");
         File[] databases = new File(args[0]).listFiles();
         File extras = new File(args[0] + "../extras/");
-        if(extras.exists() && !extendedMode) {
+        if (extras.exists() && !extendedMode) {
             databases = Stream.concat(Arrays.stream(databases), Arrays.stream(extras.listFiles())).toArray(File[]::new);
         }
         Arrays.sort(databases);
         for (File databaseLocation : databases) {
-            if(databaseLocation.isFile()) {
+            if (databaseLocation.isFile()) {
                 System.out.println("\t" + databaseLocation.getName());
                 amtPreviousSignaturesMD5 = amtSignaturesAddedMD5;
                 amtPreviousSignaturesSHA1 = amtSignaturesAddedSHA1;
@@ -172,29 +197,31 @@ public class Main {
         System.out.println("Read count: md5: " + amtSignaturesReadMD5 + ", sha1: " + amtSignaturesReadSHA1 + ", sha256: " + amtSignaturesReadSHA256);
         System.out.println("Added count: md5: " + amtSignaturesAddedMD5 + ", sha1: " + amtSignaturesAddedSHA1 + ", sha256: " + amtSignaturesAddedSHA256);
         System.out.println("Approximate count: md5: " + signaturesMD5.approximateElementCount() + ", sha1: " + signaturesSHA1.approximateElementCount() + ", sha256: " + signaturesSHA256.approximateElementCount());
-        if(extendedMode) { System.out.println("Deduped count: md5: " + amtSignaturesDedupedMD5); }
+        if (extendedMode) {
+            System.out.println("Deduped count: md5: " + amtSignaturesDedupedMD5);
+        }
         System.out.println("Max amount: md5: " + amtMaxMD5 + ", sha1: " + amtMaxSHA1 + ", sha256: " + amtMaxSHA256);
-        System.out.println("Fill amount: md5: " + ((100F/amtMaxMD5) * amtSignaturesAddedMD5) + "%, sha1: " + ((100F/amtMaxSHA1) * amtSignaturesAddedSHA1) + "%, sha256: " + ((100F/amtMaxSHA256) * amtSignaturesAddedSHA256) + "%");
+        System.out.println("Fill amount: md5: " + ((100F / amtMaxMD5) * amtSignaturesAddedMD5) + "%, sha1: " + ((100F / amtMaxSHA1) * amtSignaturesAddedSHA1) + "%, sha256: " + ((100F / amtMaxSHA256) * amtSignaturesAddedSHA256) + "%");
 
         System.out.println("App reported count: " + (signaturesMD5.approximateElementCount() + signaturesSHA1.approximateElementCount() + signaturesSHA256.approximateElementCount()));
         System.out.println("Expected false postive rate: md5: " + signaturesMD5.expectedFpp() + ", sha1: " + signaturesSHA1.expectedFpp() + ", sha256: " + signaturesSHA256.expectedFpp());
         System.out.println("Testing exclusions:");
         int matchedExclusions = 0;
-        for(String excluded : arrExclusions) {
-                if(excluded.length() == 32 && signaturesMD5.mightContain(excluded)) {
-                        System.out.println("\tmd5: Found excluded hash " + excluded);
-                        matchedExclusions++;
-                }
-                if(excluded.length() == 40 && signaturesSHA1.mightContain(excluded)) {
-                        System.out.println("\tsha1: Found excluded hash " + excluded);
-                        matchedExclusions++;
-                }
-                if(excluded.length() == 64 && signaturesSHA256.mightContain(excluded)) {
-                        System.out.println("\tsha256: Found excluded hash " + excluded);
-                        matchedExclusions++;
-                }
+        for (String excluded : arrExclusions) {
+            if (excluded.length() == 32 && signaturesMD5.mightContain(excluded)) {
+                System.out.println("\tmd5: Found excluded hash " + excluded);
+                matchedExclusions++;
+            }
+            if (excluded.length() == 40 && signaturesSHA1.mightContain(excluded)) {
+                System.out.println("\tsha1: Found excluded hash " + excluded);
+                matchedExclusions++;
+            }
+            if (excluded.length() == 64 && signaturesSHA256.mightContain(excluded)) {
+                System.out.println("\tsha256: Found excluded hash " + excluded);
+                matchedExclusions++;
+            }
         }
-        if(matchedExclusions == 0) {
+        if (matchedExclusions == 0) {
             System.out.println("\tNo exclusions found :)");
         } else {
             System.out.println("\tExclusions were found!");
@@ -211,6 +238,10 @@ public class Main {
             FileOutputStream fileSignaturesSHA256 = new FileOutputStream(new File(args[0]) + "/hypatia-sha256-bloom.bin");
             signaturesSHA256.writeTo(fileSignaturesSHA256);
             fileSignaturesSHA256.close();
+
+            FileOutputStream fileDomains = new FileOutputStream(new File(args[0]) + "/hypatia-domains-bloom.bin");
+            domains.writeTo(fileDomains);
+            fileDomains.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -227,7 +258,7 @@ public class Main {
     private static void addChecked(String potentialHash, boolean report) {
         if (!potentialHash.startsWith("#") && potentialHash.length() >= 4) {
             if (isHexadecimal(potentialHash)) {
-                if(arrExclusions.contains(potentialHash)) {
+                if (arrExclusions.contains(potentialHash)) {
                     System.out.println("\t\tSkipping excluded hash: " + potentialHash);
                     return;
                 }
@@ -237,7 +268,7 @@ public class Main {
                 if (potentialHash.length() == 32) {
                     boolean shouldAdd = true;
                     if (extendedMode) {
-                        if(signaturesMD5Dedupe.mightContain(potentialHash)) {
+                        if (signaturesMD5Dedupe.mightContain(potentialHash)) {
                             shouldAdd = false;
                             amtSignaturesDedupedMD5++;
                         }
@@ -261,11 +292,11 @@ public class Main {
                     amtLinesValid++;
                 } else {
                     amtLinesInvalid++;
-                    if(report) System.out.println("\t\tINVALID LENGTH: " + potentialHash);
+                    if (report) System.out.println("\t\tINVALID LENGTH: " + potentialHash);
                 }
             } else {
                 amtLinesInvalid++;
-                if(report) System.out.println("\t\tNOT HEXADECIMAL: " + potentialHash);
+                if (report) System.out.println("\t\tNOT HEXADECIMAL: " + potentialHash);
             }
         }
     }
