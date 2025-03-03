9742e7e6783a7bf1f226d9cd7cdb75a66cbc429d
Aliberk Sandıkçı <git@asandikci.com>
2025-03-03 23:20:40 +0300

    add nix file to use android sdk

---
+++ android-env.nix
@@ -0,0 +1,27 @@
+# see https://github.com/tadfisher/android-nixpkgs
+{ pkgs ? import <nixpkgs> { config.android_sdk.accept_license = true; } }:
+
+let
+  android-nixpkgs = pkgs.callPackage <android-nixpkgs> {
+    # Default; can also choose "beta", "preview", or "canary".
+    channel = "stable";
+  };
+
+  androidSdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
+    cmdline-tools-latest
+    build-tools-34-0-0
+    platform-tools
+    platforms-android-34
+    # emulator
+  ]);
+
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    androidSdk
+    glibc
+  ];
+
+  # Override the aapt2 that gradle uses with the nix-shipped version
+  GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/share/android-sdk/build-tools/34.0.0/aapt2";
+}
